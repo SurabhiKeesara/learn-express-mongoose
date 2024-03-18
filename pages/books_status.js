ccfc784ac925f29f5a61917295363aa4ef5ed178
SurabhiKeesara <surabhi.keesara@gmail>
2024-03-18 16:09:47 -0400

    class activities author and availability

--- pages/books_status.js
+++ pages/books_status.js
@@ -1,5 +1,18 @@
 let BookInstance = require('../models/bookinstance');
 
+function get_books_status () {
+  return Book.find({}, 'title status')
+    .sort({title : 1})  // 1 indictes ascending order
+}
+
 exports.show_all_books_status = function(res) {
-  return res.send([]);
+  BookInstance.find({'status': {$eq: 'Available'}})
+  .populate('book')
+  .exec()
+  .then(list => {
+    res.send(list.map(function(bi) {
+      return bi.book.title + " : " + bi.status
+    }))
+  })
+  .catch(err => console.log(err.message))
 }
